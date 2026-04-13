daaef64a90364eb22e4bb06ff40a940c99f2cd4e
olimov-web <abubakirolimov392@gmail.com>
2026-04-13 10:04:08 +0500

    first

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <header>
+      <div className="logo">
+       <h1 style={{fontSize: '25px'}}>GYM <span style={{ color: '#FAFF00' }}>24</span></h1> 
+       <nav>
+        <ul>
+          <li>Advantages</li>
+          <li>Membership</li>
+          <li>About</li>
+          <li>Trainers</li>
+        </ul>
+       </nav>
+      </div>
+      </header>
+    </div>
+    
   )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import './Main.css'
+import img from '../../assets/oka.png'
+import img1 from '../../assets/card1.png'
+import img2 from '../../assets/card2.png'
+import img3 from '../../assets/card3.png'
+import img4 from '../../assets/card4.png'
+
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <main>
+        <div className="left">
+      <h1>Free trial session <br /> with a trainer</h1>
+      <button className='btn1'>Detailed</button>
+      </div>
+
+        <img src={img} alt="" />
+
+      </main>
+              <h1 style={{backgroundColor:"black", color:"white", paddingLeft:'220px',paddingBottom:"20px"}}>Reasons to join</h1>
+      <div className="main">
+        <div className="left1">
+
+      <div style={{marginLeft:"-40px"}} className="card1">
+        <img src={img1} alt="" />
+<div className="yozu">
+          <h5>15000 sq.m.</h5>
+        <p>A spacious gym for sports-a safe distance <br />between exeprice machines</p>
+</div>
+      </div>
+      <div style={{marginTop:"30px"}} className="card1">
+      <img src={img2} alt="" />
+    <div className="yozu">
+            <h5>4 fitness zones</h5>
+      <p>From cardio to functional and cycle. Separate area <br /> for boxing and mixed martial arts</p>
+    </div>
+      </div>
+        </div>
+        <div className="right1">
+          <div className="card1">
+            <img src={img3} alt="" />
+<div className="yozu">
+              <h5>More than 200 equipment</h5>
+            <p>No queues at the simulators. Premium equipment <br /> from LifeStyle, Hammer Strength, TechnoGym.</p>
+</div>
+
+          </div>
+          <div style={{marginTop:"20px"}} className="card1">
+            <img src={img4} alt="" />
+<div className="yozu">
+              <h5>Round-the-clock operation</h5>
+            <p>The gym is open 24 hours a day, also works on all <br />
+             holidays and weekends</p>
+</div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
